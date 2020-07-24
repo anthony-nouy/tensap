891d018f4ee74fb10de9dd4a6a113835fa8b77f5
Julien Schueller <schueller@phimeca.com>
2020-07-24 17:07:48 +0200

    Move license headers out of docstrings

--- tensap/functions/multivariate_functions_benchmark.py
+++ tensap/functions/multivariate_functions_benchmark.py
@@ -1,21 +1,21 @@
+# Copyright (c) 2020, Anthony Nouy, Erwan Grelier
+# This file is part of tensap (tensor approximation package).
+
+# tensap is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tensap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with tensap.  If not, see <https://www.gnu.org/licenses/>.
+
 '''
 Module multivariate_functions_benchmark
-
-Copyright (c) 2020, Anthony Nouy, Erwan Grelier
-This file is part of tensap (tensor approximation package).
-
-tensap is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-tensap is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with tensap.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
 
