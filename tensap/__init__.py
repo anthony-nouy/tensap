f540bf4b0a9806bb973885289f076a0fd439b745
Anthony Nouy <anouy@wlan-036068.eduroam.tue.nl>
2023-07-10 11:58:55 +0200

    v1.5

--- tensap/__init__.py
+++ tensap/__init__.py
@@ -21,7 +21,7 @@ Initialization file for tensap (tensor approximation package).
 
 # flake8: noqa
 
-__version__ = "1.4"
+__version__ = "1.5"
 
 from .linear_algebra.magic_indices import magic_indices
 
