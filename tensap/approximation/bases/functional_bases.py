fix: prevent list slicing error with `FunctionalBases.derivative`
`FunctionalBases.derivative(n).bases` attribute was a list. This resulted in list slicing error in multiple methods involving slicing by an array.
For example `FunctionalBases.derivative(n).eval(x)` raised an error due to `self.bases[dims]`.

Fixed by transforming `FunctionalBases.derivative(n).bases` into an `numpy.ndarray`.

tensap/approximation/bases/functional_bases.py
@@ -349,7 +349,7 @@ class FunctionalBases:
 
         """
         out = deepcopy(self)
-        out.bases = [x.derivative(m) for x, m in zip(self.bases, n)]
+        out.bases = np.array([x.derivative(m) for x, m in zip(self.bases, n)])
         return out
 
     def random(self, *args, **kwargs):