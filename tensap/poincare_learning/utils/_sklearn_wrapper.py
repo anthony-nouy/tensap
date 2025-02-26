7a659db96eeb04e2077352185c56dbbfed2f628b
alexandre-pasco <alexandre.pasco1702@gmail.com>
2025-02-26 10:26:43 +0100

    change: CV learning only for feature

--- tensap/poincare_learning/utils/_sklearn_wrapper.py
+++ tensap/poincare_learning/utils/_sklearn_wrapper.py
@@ -1,49 +1,35 @@
 
 
 import numpy as np
-from sklearn.base import RegressorMixin, BaseEstimator, clone
-from sklearn.metrics import mean_squared_error
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.model_selection import GridSearchCV
-from tensap.poincare_learning.benchmarks.poincare_benchmarks_torch import build_benchmark_torch
+from sklearn.base import RegressorMixin, BaseEstimator
 from tensap.poincare_learning.utils._loss_vector_space import _build_ortho_poly_basis
 from tensap.poincare_learning.poincare_loss_vector_space import PoincareLossVectorSpace
+from tensap.approximation.bases.sub_functional_basis import SubFunctionalBasis
+import logging
 
 
-
-class PoincareEstimator(RegressorMixin, BaseEstimator):
+class PolynomialFeatureEstimator(BaseEstimator):
     """
-    Class PoincareEstimator. Wraps a Poincare Inequality based approximation
+    Class PolynomialFeatureEstimator. Wraps a Poincare Inequality based feature learning
     method into a sklearn estimator, which allows to use sklearns learning
     algorithms such as GridSearchCV.
-    The approximation method is of the form f(g(x)) with g the so-called 
-    feature map, and f the so-called profile or regression function.
-    Here g have the form g(x) = G^T Phi(x) where Phi:R^d -> R^K is an orthonormal
+    The features learnt have the form g(x) = G^T Phi(x) where Phi:R^d -> R^K is an orthonormal
     polynomial basis wrt the L2 inner product, whose degree multi-indices have
-    bounded p_norm. G has shape (K, m) and is orthonormal, ie G^T G = I_m. 
-    f is a Kernel Ridge Regression function.
+    bounded p_norm. G has shape (K, m) and is orthonormal, ie G^T G = I_m.
 
     Attributes
     ----------
+    random_vector : RandomVector
+        Instance of RandomVector with respect to which the polynomial basis 
     p_norm : float
         The p-norm used for the degrees, either a positive real scalar or numpy.inf.
     max_p_norm : float
         The bound of the norm, a positive real scalar.
     innerp : string
         The orthogonality condition wrt which the matrix G should be orthonormal
-        during the learning procedure. Note that G is re-orthonormalized wrt
-        euclidean inner product after learning, in order to stabilize the regression f.
-        Must be one of 'l2' or 'h1_0'.
-    alpha : float
-        Parameter in the kernel ridge regression. For more details see sklearn documentation
-    gamma : float
-        Parameter in the kernel ridge regression. For more details see sklearn documentation
-    benchmark_name : string
-        Name of the benchmark considered. It is used to obtained the random vector X
-        wrt which the basis Phi should be orthonormal.
-    benchmark_kwargs : dict, optional
-        Additional key word arguments for the benchmark, such as input dim, etc.
-        The default is {}
+        during the learning procedure. Then G is re-orthonormalized wrt
+        euclidean inner product after learning, so that G^T Phi(X) is standardised.
+        Should be one of 'l2' or 'h1_0'.
     fit_method : string, optional
         The method used to learn the feature map, ie to learn G.
         Can be one of 'pymanopt', 'qn', 'surrogate' or 'surrogate_greedy'.
@@ -55,37 +41,19 @@ class PoincareEstimator(RegressorMixin, BaseEstimator):
 
     """
 
-    def __init__(self, p_norm, max_p_norm, innerp, alpha, gamma, benchmark_name, benchmark_kwargs={}, 
-                 fit_method='pymanopt', fit_parameters={}, krr_param_grid={}):
+    def __init__(self, random_vector, p_norm, max_p_norm, innerp, fit_method='pymanopt', fit_parameters={}):
         
+        self.random_vector = random_vector
         self.p_norm = p_norm  
         self.max_p_norm = max_p_norm
         self.innerp = innerp
-        self.alpha = alpha
-        self.gamma = gamma
-        self.benchmark_name = benchmark_name
-        self.benchmark_kwargs = benchmark_kwargs
         self.fit_method = fit_method
         self.fit_parameters = fit_parameters
-        self.krr_param_grid = krr_param_grid
-
-        self.G = None
-
-        # get the random vector input associated to the benchmark
-        _, _, rand_vec = build_benchmark_torch(benchmark_name, **benchmark_kwargs)
-        self.rand_vec = rand_vec
 
         # build orthonormal basis
-        self.basis = _build_ortho_poly_basis(rand_vec, p_norm, max_p_norm)
-
-        # initialise kernel ridge reressor
-        self.regressor = cv_model = GridSearchCV(
-            KernelRidge(kernel="rbf"),
-            param_grid=krr_param_grid,
-            scoring="neg_mean_squared_error",
-            verbose=0,
-            cv=10
-        )
+        self.basis = _build_ortho_poly_basis(random_vector, p_norm, max_p_norm)
+        self.G = None
+        self.g = None
 
         # Compute the inner pdocut matrix if necessary
         if self.innerp == "l2":
@@ -93,25 +61,22 @@ class PoincareEstimator(RegressorMixin, BaseEstimator):
         elif self.innerp == "h1_0":
             R = self.basis.gram_matrix_h1_0()
         else:
-            print("Warning : orthogonality condition for G not valid, identity matrix is taken by default.")
+            logging.warning("Warning : orthogonality condition for G not valid, identity matrix is taken by default.")
             R = None
         
         self.R = R
 
 
-    def fit(self, X, y, jac):
+    def fit(self, X, jac_u):
         """
-        Fit the coefficients G of the feature map in the fixed basis and the regression function.
+        Fit the coefficients G of the feature map in the fixed basis.
 
         Parameters
         ----------
         X : numpy.ndarray
             Sample points.
             Has shape (N, d).
-        y : numpy.ndarray
-            Evaluations of u at sample points.
-            Has shape (N, n) of (N, n).
-        jac : numpy.ndarray
+        jac_u : numpy.ndarray
             Evaluations of jac_u at sample points.
             jac_u[k,i,j] is du_i / dx_j evaluated at the k-th sample.
             Has shape (N, n, d).
@@ -121,33 +86,11 @@ class PoincareEstimator(RegressorMixin, BaseEstimator):
         self : PoincareEstimator
             The fitted estimator.
         """
+
         # Learning the feature map by minizming Poincare loss with pymanopt
         ploss = PoincareLossVectorSpace(
-            jac, self.basis.eval_jacobian(X), self.basis, self.R)
-        G = self._fit_features(ploss)
-
-        # Re-orthonormalize G wrt euclidean inner product
-        G = np.linalg.svd(G, full_matrices=False)[0]
-        self.G = G
-
-        # learning the kernel ridge regressor on the features
-    
-        Z = self.basis.eval(X) @ G
-        self.regressor.fit(Z, y)
-
-        return self
-
-
-    def predict(self, X):
-
-        Z = self.basis.eval(X) @ self.G
-        Y = self.regressor.predict(Z)
-
-        return Y
-    
-    
-    def _fit_features(self, ploss):
-
+            jac_u, self.basis.eval_jacobian(X), self.basis, self.R)
+        
         if self.fit_method == 'pymanopt':
             minimizer = ploss.minimize_pymanopt
         elif self.fit_method == 'qn':
@@ -156,11 +99,21 @@ class PoincareEstimator(RegressorMixin, BaseEstimator):
             minimizer = ploss.minimize_surrogate
         else:
             raise NotImplementedError('Method not implemented')
-        
+            
         G, losses = minimizer(**self.fit_parameters)[:2]
-        
-        if G.ndim == 3:
-            G = G[losses.argmin()]
 
-        return G
+        # Re-orthonormalize G wrt euclidean inner product
+        G = np.linalg.svd(G, full_matrices=False)[0]
+        self.G = G
+        self.g = SubFunctionalBasis(self.basis, G)
+
+        return self
     
+    
+    def score(self, X, jac_u):
+
+        ploss = PoincareLossVectorSpace(
+            jac_u, self.basis.eval_jacobian(X), self.basis, self.R)
+
+        return ploss.eval(self.G)
+
