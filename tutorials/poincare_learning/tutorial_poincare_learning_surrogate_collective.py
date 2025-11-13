f702166d3b113ae70084098ef9fc4e0818270612
alexandre-pasco <alexandre.pasco1702@gmail.com>
2025-11-13 11:34:28 +0100

    fix: handle the case where pymanopt is not installed

--- tutorials/poincare_learning/tutorial_poincare_learning_surrogate_collective.py
+++ tutorials/poincare_learning/tutorial_poincare_learning_surrogate_collective.py
@@ -207,8 +207,6 @@ x_test, u_test, _, _, _, loss_test = generate_samples(
 m = len(mat_lst) - 0
 loss_train_tensorized.truncate(m)
 G_surr, _, _ = loss_train_tensorized.minimize_surrogate(m=m)
-# G_surr, _, _ = loss_train_tensorized.minimize_pymanopt(G0=G_surr,
-#   optimizer_kwargs={'max_iterations':100})
 G_surr = np.linalg.svd(G_surr, full_matrices=False)[0]
 
 # %% Evaluate performances
@@ -292,6 +290,106 @@ plt.title(f"""
 plt.show()
 
 ##############################################################
+# %% (Optional) Run Minimization of the Poincare loss on the grassmann manifold
+##############################################################
+
+# CG on grassmann manifold using pymanopt, when installed
+try:
+    optimizer_kwargs = {  # params for pymanopt CG
+        'beta_rule': 'PolakRibiere',
+        'max_iterations': 50,
+        'verbosity': 2,
+    }
+    G_opt, _, _ = loss_train_tensorized.minimize_pymanopt(
+        G_surr, use_precond=True, optimizer_kwargs=optimizer_kwargs
+    )
+
+# quasi newton
+except ImportError:
+    G_opt, _ = loss_train_tensorized.minimize_qn(G0=G_surr, m=m, maxiter=50, tol=1e-10)
+
+
+# %% Evaluate performances
+
+print(f"\nPoincare loss and Surrogate as init on {G_opt.shape[1]} features")
+print(f"Surrogate on tensorized train set:      {loss_train_tensorized.eval_surrogate(G_opt):.3e}")
+print(f"Poincare loss on tensorized train set : {loss_train_tensorized.eval(G_opt):.3e}")
+print(f"Poincare loss on test set:              {loss_test.eval(G_opt):.3e}")
+
+# %% Plot for eyeball regression
+
+z_opt_train = basis.eval(x_train[:, ind1]) @ G_opt
+z_opt_test = basis.eval(x_test[:, ind1]) @ G_opt
+
+fig, ax = plt.subplots(1, z_opt_train.shape[1])
+if z_opt_train.shape[1] == 1:
+    ax = [ax]
+ax[0].set_ylabel('u(X)')
+
+for i in range(z_opt_train.shape[1]):
+    for j in range(N1_train):
+        ax[i].scatter(z_opt_train[j::N2_train, i], u_train[j::N2_train], label='train', s=5.)
+    ax[i].set_xlabel(f'g_{i}(X)')
+
+fig.suptitle(f"""
+    Surrogate as init tensorized sample
+    Poly features m={z_opt_train.shape[1]}
+    Multi-indices with {p_norm}-norm bounded by {max_deg}
+    {N1_train}x{N2_train}={N1_train * N2_train} train samples
+    """,
+    y=0.)
+plt.show()
+
+
+# %% Fit regressor with sklearn
+
+# add the last parameter as a feature
+zy_opt_train = np.hstack([z_opt_train, x_train[:, ind2]])
+zy_opt_test = np.hstack([z_opt_test, x_test[:, ind2]])
+
+# shuffle = np.random.permutation(len(zy_opt_train))
+shuffle = np.arange(len(zy_opt_train))
+
+# regressor_opt = fit_krr_regressor(zy_opt_train[shuffle], u_train[shuffle])
+regressor_opt = fit_poly_regressor(zy_opt_train[shuffle], u_train[shuffle])
+
+
+def f_opt(z):
+    return regressor_opt.predict(z)
+
+
+# %% Evaluate performances
+
+y_train = f_opt(zy_opt_train).reshape(u_train.shape)
+err_train = np.sqrt(np.mean((y_train - u_train)**2))
+rel_err_train = err_train / np.sqrt((u_train**2).mean())
+
+y_test = f_opt(zy_opt_test).reshape(u_test.shape)
+err_test = np.sqrt(np.mean((y_test - u_test)**2))
+rel_err_test = err_test / np.sqrt((u_test**2).mean())
+
+print(f"\nSurrogate as init tensorized sample | Regression based on {G_opt.shape[1]} features")
+print(f"L2 on train set    : {err_train:.3e}")
+print(f"L2 on test set     : {err_test:.3e}")
+print(f"RL2 on train set   : {rel_err_train:.3e}")
+print(f"RL2 on test set    : {rel_err_test:.3e}")
+
+
+# %% Plot final regression
+
+plt.scatter(y_test, u_test, label='test', s=5.)
+plt.scatter(y_train, u_train, label='train', s=5.)
+plt.ylabel("u(X)")
+plt.xlabel("f(g(X))")
+plt.legend()
+plt.title(f"""
+    Surrogate as init tensorized sample
+    Poly features m={zy_opt_train.shape[1]}
+    Multi-indices with {p_norm}-norm bounded by {max_deg}
+    {N1_train}x{N2_train}={N1_train * N2_train} train samples""")
+plt.show()
+
+##############################################################
 # %% Compare with non tensorized sample and classical approach
 ##############################################################
 
@@ -300,8 +398,21 @@ x_train, u_train, _, _, _, loss_train = generate_samples(
     N1_train * N2_train, ind1, X, u, jac_u, basis, R)
 
 # %% minimize the poincare based loss function
-G_pmo, _, _ = loss_train.minimize_pymanopt(
-    m=m, init_method='active_subspace', optimizer_kwargs={'max_iterations': 100})
+
+# CG on grassmann manifold using pymanopt, when installed
+try:
+    optimizer_kwargs = {  # params for pymanopt CG
+        'beta_rule': 'PolakRibiere',
+        'max_iterations': 100,
+        'verbosity': 2,
+    }
+    G_pmo, _, _ = loss_train.minimize_pymanopt(
+        m=m, init_method='active_subspace', optimizer_kwargs={'max_iterations': 100})
+
+# quasi newton
+except ImportError:
+    G_pmo, _ = loss_train.minimize_qn(
+        G0=None, m=m, init_method='active_subspace', maxiter=50, tol=1e-10)
 
 # %% Evaluate performances
 
